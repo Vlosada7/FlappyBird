beb5898b50f34d43d589921022a665b20e1e1dc0
Victor Losada <117365425+Vlosada7@users.noreply.github.com>
2023-05-27 00:31:57 +0200

    Dead and Score

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const canvas = document.getElementById("canvas");
 const ctx = canvas.getContext("2d");
 
-//Load game images 
+//Load game images
 const bird = new Image();
 bird.src = "images/bird.png";
 
@@ -17,14 +17,14 @@ pipeUp.src = "images/canocima.png";
 const pipeDown = new Image();
 pipeDown.src = "images/canobaixo.png";
 
-//Variables 
+//Variables
 
 const spaceBetween = 100;
-let constant; 
-let bX = 33; 
+let constant;
+let bX = 33;
 let bY = 200;
 const gravity = 1.4;
-let score = 0; 
+let score = 0;
 let pipes = [];
 
 pipes[0] = {
@@ -44,30 +44,48 @@ document.addEventListener("keydown", fly);
 
 //Fly function
 function fly() {
-  bY -= 26;
-  flyAudio.play();
+	bY -= 26;
+	flyAudio.play();
 }
 
 function game() {
 	//Background (imagem, posição X, posição Y)
 	ctx.drawImage(bg, 0, 0);
 
-  //Creating pipes
-  for (let i = 0; i < pipes.length; i++) {
-    //Position down pipe
-    constant = pipeUp.height + spaceBetween;
-    //Position down pipe
-    ctx.drawImage(pipeUp, pipes[i].x, pipes[i].y);
-    //config down pipe
-    ctx.drawImage(pipeDown, pipes[i].x, pipes[i].y + constant);
-    //Pipe movement
-    pipes[i].x = pipes[i].x - 1; 
+	//Creating pipes
+	for (let i = 0; i < pipes.length; i++) {
+		//Position down pipe
+		constant = pipeUp.height + spaceBetween;
 
-    if (pipes[i].x == 125) {
-      
+		//Position down pipe
+		ctx.drawImage(pipeUp, pipes[i].x, pipes[i].y);
+
+		//config down pipe
+		ctx.drawImage(pipeDown, pipes[i].x, pipes[i].y + constant);
+
+		//Pipe movement
+		pipes[i].x = pipes[i].x - 1;
+
+		//Create new pipes
+		if (pipes[i].x == 100) {
+			pipes.push({
+				x: canvas.width,
+				y: Math.floor(Math.random() * pipeUp.height) - pipeUp.height,
+			});
+		}
+
+		//Colision between pipes
+    if ((bX +bird.width >= pipes[i].x && bX <= pipes[i].x +pipeUp.width) && (bY <= pipes[i].y + pipeUp.height || bY + bird.height >= pipes[i].y + constant) || (bY + bird.height >= canvas.height - floor.height)) {
+      location.reload();
     }
 
-  }
+    //Score system
+    if (pipes[i].x == 5) {
+      score += 1;
+      scoreAudio.play();
+    }
+			
+	}
 
 	//Floor
 	ctx.drawImage(floor, 0, canvas.height - floor.height + 2);
@@ -75,6 +93,11 @@ function game() {
 	//Bird
 	ctx.drawImage(bird, bX, bY);
 	bY += gravity;
+
+  //Score board
+  ctx.fillStyle = "#000";
+  ctx.font = "20px Verdana";
+  ctx.fillText("Score: " + score, 10, canvas.height - 20);
 
 	requestAnimationFrame(game);
 }
